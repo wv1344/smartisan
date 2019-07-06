75d945a657437cabdf6d8136882b493bd44bee5b
wv1344 <wv1344@gmail.com>
2019-07-06 16:39:53 +0800

    start

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <!-- <i-tabbar class="tabbarWrapper"></i-tabbar> -->
+    <div class="tabbar">
+      <router-link to="/" class="bar">
+        <div class="home">
+          <i class="iconfont icon-shouye-copy bg"></i>
+          <span class="text">首页</span>
+        </div>
+      </router-link>
+      <router-link to="/classify" class="bar">
+        <div class=" classify">
+          <i class="iconfont icon-fenlei-copy bg"></i>        
+          <span class="text">分类</span>
+        </div>
+      </router-link>
+      <router-link to="" class="bar">
+        <div class=" cart">
+          <i class="iconfont icon-gouwu-copy bg"></i>        
+          <span class="text">购物车</span>
+        </div>
+      </router-link>
+      <router-link to="" class="bar">
+        <div class=" user">
+          <i class="iconfont icon-gerenzhongxin-copy bg"></i>        
+          <span class="text">个人中心</span>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+#app
+  margin 0
+  padding 0
+  width 100%
+  height 100%
+  .tabbar
+    position fixed
+    bottom  0px
+    display flex
+    left 0
+    right 0
+    .bar
+      text-decoration none
+      height 50px
+      flex 1
+      background #fff
+      text-align center
+      padding-top 5px
+      .bg
+        display block
+        font-size 25px
+        color #CECECE
+      .text
+        font-size 12px
+        color #d7d7d7
 </style>
---
+++ src/components/classify.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="classify">
+    sdfdsfa
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
